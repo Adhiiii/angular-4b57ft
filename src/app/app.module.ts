f8428f09a4f0267ab1ecfec249e78ae9ef6ef452
Adhiiii <adheenrockz@gmail.com>
2019-10-15 20:27:57 +0530

    1 file modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { RouterModule, Routes } from '@angular/router';
 
 import { ListEmployeesComponent } from './employees/list-employees.component';
 import { CreateEmployeesComponent } from './employees/create-employee.component';
-
-
 import { AppComponent } from './app.component';
 
+const appRoutes: Routes = [
+  {path: 'list', component: ListEmployeesComponent },
+  {path: 'create', component: CreateEmployeesComponent },
+  {path: '', redirectTo: '/list' }
+]
+
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
+  imports:      [ BrowserModule, FormsModule, RouterModule ],
   declarations: [ AppComponent, ListEmployeesComponent ],
   bootstrap:    [ AppComponent ]
 })
