4e144f1e445dcc307c2c92ff3c481834dcbce87c
Adhiiii <adheenrockz@gmail.com>
2019-10-19 17:37:53 +0530

    6 files modified

--- src/app/employees/create-employee.component.ts
+++ src/app/employees/create-employee.component.ts
@@ -8,6 +8,8 @@ import { NgForm } from '@angular/forms';
 })
 export class CreateEmployeesComponent implements OnInit {
 
+  gender = 'male';
+  cntctpref = 'email';
   constructor() { }
 
   ngOnInit() { }
